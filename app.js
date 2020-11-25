998f3a248b7839e32234e548f6cba69bdbe23063
Dimitri Wahyudiputra <gitmitri@gmail.com>
2020-11-25 19:48:20 +0700

    Add cors middleware

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 const { Protagonist } = require('./models/');
+
+app.use(cors());
 
 app.get('/protagonists', async function (req, res, next) {
   try {
